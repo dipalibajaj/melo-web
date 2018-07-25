9a03bc77db5e6aeac9670b1f885dad63c1606343
Dipali Bajaj <dipalibajaj9@gmail.com>
2018-07-25 16:25:31 -0700

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>For life's ups & downs.</h1>
-    <p>A community of humans to support you in your journey of life. Through your struggles, and happy moments, we are here for you!</p>
-    <Link to="/page-2/">Watch Vido</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>For life's ups & downs.</h1>
+        <p>A community to support you in your journey of life. Through your struggles and happy moments, we are here for you!</p>
+        <Link to="/page-2/">Watch Video</Link>
+      </div>
+    </div>
   </div>
 )
 
