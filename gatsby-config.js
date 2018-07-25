5405ecf82b99e4011eeac86574fcc94d11a4d346
Dipali Bajaj <dipalibajaj9@gmail.com>
2018-07-25 14:17:52 -0700

    Starter

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Melo',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>For life's ups & downs.</h1>
+    <p>A community of humans to support you in your journey of life. Through your struggles, and happy moments, we are here for you!</p>
+    <Link to="/page-2/">Watch Video</Link>
+  </div>
+)
+
+export default IndexPage
